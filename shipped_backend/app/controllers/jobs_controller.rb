8b4c179c76b4401b75e757661e5ea8663e044a88
tomatoandcheerio <Georxia@gmail.com>
2017-06-07 13:34:36 -0400

    added routes in controller and certain create syntax in the Controller

--- shipped_backend/app/controllers/jobs_controller.rb
+++ shipped_backend/app/controllers/jobs_controller.rb
@@ -1,2 +1,12 @@
 class JobsController < ApplicationController
+    def create
+
+
+    end
+
+    private
+
+    def job_params
+        params.require(:).permit(:name, :password)
+    end
 end
--- shipped_backend/app/controllers/users_controller.rb
+++ shipped_backend/app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def index
+
+        render json: User.all
+    end
+
+    def create
+
+
+    end
+
+    private
+
+    def user_params
+        params.require(:).permit(:name, :password)
+    end
 end
